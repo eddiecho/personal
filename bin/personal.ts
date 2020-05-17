b123655b8b516e650a04cdff4dd283e385b6b6cf
Edward Cho <eunseocho@gmail.com>
2020-05-17 10:38:25 -0700

    finish copying the codepipeline example use a github source instead of codecommit use secrets manager to fetch oauth creds

--- bin/personal.ts
+++ bin/personal.ts
@@ -1,7 +1,24 @@
 #!/usr/bin/env node
 import "source-map-support/register";
-import * as cdk from "@aws-cdk/core";
-import { PersonalStack } from "../lib/personal-stack";
+import * as Cdk from "@aws-cdk/core";
+import * as SecretsManager from "aws-sdk/clients/secretsmanager";
 
-const app = new cdk.App();
-new PersonalStack(app, "PersonalStack");
+import { PersonalStack } from "../lib/personal-stack";
+import { DeployStack } from "../lib/deploy-stack";
+import { getSecret } from "../lib/secrets";
+
+(async function () {
+  const app = new Cdk.App();
+  const personalStack = new PersonalStack(app, "PersonalStack");
+
+  const githubSecret: SecretsManager.DescribeSecretResponse = await getSecret(
+    "GithubPersonalAccessToken"
+  );
+  new DeployStack(app, "DeployStack", {
+    // overly strict aliasing
+    GithubSecretArn: githubSecret.ARN as string,
+    LambdaCode: personalStack.lambdaCode,
+  });
+
+  app.synth();
+})();
--- lib/deploy-stack.ts
+++ lib/deploy-stack.ts
@@ -1,9 +1,14 @@
 import * as CodeBuild from "@aws-cdk/aws-codebuild";
 import * as CodePipeline from "@aws-cdk/aws-codepipeline";
 import * as CodePipelineActions from "@aws-cdk/aws-codepipeline-actions";
-import { App, Stack, StackProps } from "@aws-cdk/core";
+import * as Lambda from "@aws-cdk/aws-lambda";
+import * as SecretsManager from "@aws-cdk/aws-secretsmanager";
+import { App, SecretValue, Stack, StackProps } from "@aws-cdk/core";
 
-export interface DeployStackProps extends StackProps {}
+export interface DeployStackProps extends StackProps {
+  readonly GithubSecretArn: string;
+  readonly LambdaCode: Lambda.CfnParametersCode;
+}
 
 export class DeployStack extends Stack {
   constructor(app: App, id: string, props: DeployStackProps) {
@@ -50,25 +55,64 @@ export class DeployStack extends Stack {
       },
     });
 
+    const sourceAuth = SecretsManager.Secret.fromSecretAttributes(
+      this,
+      "GithubSecret",
+      {
+        secretArn: props.GithubSecretArn,
+      }
+    );
     const sourceOutput = new CodePipeline.Artifact();
-    const cdkBuildOutput = new CodePipeline.Artifact('CdkBuildOutput');
-    const lambdaBuildOutput = new CodePipeline.Artifact('LambdaBuildOutput');
-    new CodePipeline.Pipeline(this, 'Pipeline', {
-        stages: [
-            {
-                stageName: 'Source',
-                actions: [
-                    new CodePipelineActions.GitHubSourceAction({
-                        actionName: "GithubSource",
-                        output: sourceOutput,
-                        repo: 'personal',
-                        branch: 'master',
-                        owner: 'eddiecho',
-                        oauthToken: 'this will fail'
-                    })
-                ]
-            }
-        ]
-    })
+    const cdkBuildOutput = new CodePipeline.Artifact("CdkBuildOutput");
+    const lambdaBuildOutput = new CodePipeline.Artifact("LambdaBuildOutput");
+    new CodePipeline.Pipeline(this, "Pipeline", {
+      stages: [
+        {
+          stageName: "Source",
+          actions: [
+            new CodePipelineActions.GitHubSourceAction({
+              actionName: "GithubSource",
+              output: sourceOutput,
+              repo: "personal",
+              branch: "master",
+              owner: "eddiecho",
+              oauthToken: sourceAuth,
+            }),
+          ],
+        },
+        {
+          stageName: "Build",
+          actions: [
+            new CodePipelineActions.CodeBuildAction({
+              actionName: "LambdaBuild",
+              project: lambdaBuild,
+              input: sourceOutput,
+              outputs: [lambdaBuildOutput],
+            }),
+            new CodePipelineActions.CodeBuildAction({
+              actionName: "CDKBuild",
+              project: cdkBuild,
+              input: sourceOutput,
+              outputs: [cdkBuildOutput],
+            }),
+          ],
+        },
+        {
+          stageName: "Deploy",
+          actions: [
+            new CodePipelineActions.CloudFormationCreateUpdateStackAction({
+              actionName: "LambdaCfnDeploy",
+              templatePath: cdkBuildOutput.atPath("LambdaStack.template.json"),
+              stackName: "LambdaStack",
+              adminPermissions: true,
+              parameterOverrides: {
+                ...props.lambdaCode.assign(lambdaBuildOutput.s3Location),
+              },
+              extraInputs: [lambdaBuildOutput],
+            }),
+          ],
+        },
+      ],
+    });
   }
 }
